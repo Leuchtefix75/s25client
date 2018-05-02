8a0b8374a9e64bcb6748c02a1f450b7c6f898bfb
Leuchtefix75 <36813135+Leuchtefix75@users.noreply.github.com>
2018-05-02 23:51:15 +0200

    Update linux.cmake

--- cmake/linux.cmake
+++ cmake/linux.cmake
@@ -11,6 +11,11 @@ endif(CCACHE_FOUND)
 
 # set compiler flags
 IF(RTTR_ENABLE_OPTIMIZATIONS)
+  IF("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "armv7l")
+	FORCE_ADD_FLAGS(CMAKE_C_FLAGS -Ofast -mcpu=cortex-a7 -mfpu=neon-vfpv4)
+	FORCE_ADD_FLAGS(CMAKE_CXX_FLAGS -Ofast -mcpu=cortex-a7 -mfpu=neon-vfpv4)
+  ELSE()
 	FORCE_ADD_FLAGS(CMAKE_C_FLAGS -ffast-math -mmmx -msse -mfpmath=sse -ggdb)
 	FORCE_ADD_FLAGS(CMAKE_CXX_FLAGS -ffast-math -mmmx -msse -mfpmath=sse -ggdb)
+  ENDIF()	
 ENDIF(RTTR_ENABLE_OPTIMIZATIONS)
